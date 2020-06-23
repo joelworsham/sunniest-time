803e6e961a7b5dcba0b6f4aa7832d4d1cfff6931
joelworsham <joelworsham@gmail.com>
2020-06-23 15:22:44 -0400

    adjust hours

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const getSunniestTimes = require('./lib/getSunniestTimes');
 
 (async () => {
   const apiUrl = await getLocationApiUrl({ lat: 39.765977, lon: -86.060360 });
-  const sunniestTimes = await getSunniestTimes(apiUrl, { minHour: 10, maxHour: 17 });
+  const sunniestTimes = await getSunniestTimes(apiUrl, { minHour: 12, maxHour: 17 });
 
   console.log(sunniestTimes);
 })();
