5fbe80e73beb267d5fc80663641b7dcf0579c39a
joelworsham <joelworsham@gmail.com>
2020-06-23 12:40:22 -0400

    working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const getLocationApiUrl = require('./lib/getLocationApiUrl');
 const getSunniestTimes = require('./lib/getSunniestTimes');
 
 (async () => {
-  const apiUrl = await getLocationApiUrl({ lat: 39.765940, lon: -86.060310 });
+  const apiUrl = await getLocationApiUrl({ lat: 39.765977, lon: -86.060360 });
   const sunniestTimes = await getSunniestTimes(apiUrl, { minHour: 10, maxHour: 17 });
 
   console.log(sunniestTimes);
